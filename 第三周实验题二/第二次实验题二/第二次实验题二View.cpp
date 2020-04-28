f6807136dd3c1f89c8349fc8ccd3693691ecf383
my607020 <1693267656@qq.com>
2020-04-28 20:50:45 +0800

    "在文档类中定义两个int类型数据成员A和B并适当初始化(例如A=1,B=2)， 鼠标左键按下时，在客户区显示"A+B=3""

---
+++ 第三周实验题二/第二次实验题二/第二次实验题二View.cpp
@@ -0,0 +1,96 @@
+
+// 第二次实验题二View.cpp : C第二次实验题二View 类的实现
+//
+
+#include "stdafx.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "第二次实验题二.h"
+#endif
+
+#include "第二次实验题二Doc.h"
+#include "第二次实验题二View.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// C第二次实验题二View
+
+IMPLEMENT_DYNCREATE(C第二次实验题二View, CView)
+
+BEGIN_MESSAGE_MAP(C第二次实验题二View, CView)
+	ON_WM_LBUTTONDOWN()
+END_MESSAGE_MAP()
+
+// C第二次实验题二View 构造/析构
+
+C第二次实验题二View::C第二次实验题二View()
+{
+	// TODO: 在此处添加构造代码
+
+}
+
+C第二次实验题二View::~C第二次实验题二View()
+{
+}
+
+BOOL C第二次实验题二View::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CView::PreCreateWindow(cs);
+}
+
+// C第二次实验题二View 绘制
+
+void C第二次实验题二View::OnDraw(CDC* /*pDC*/)
+{
+	C第二次实验题二Doc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 在此处为本机数据添加绘制代码
+}
+
+
+// C第二次实验题二View 诊断
+
+#ifdef _DEBUG
+void C第二次实验题二View::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void C第二次实验题二View::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+C第二次实验题二Doc* C第二次实验题二View::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(C第二次实验题二Doc)));
+	return (C第二次实验题二Doc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// C第二次实验题二View 消息处理程序
+
+
+void C第二次实验题二View::OnLButtonDown(UINT nFlags, CPoint point)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+	C第二次实验题二Doc* pDoc = GetDocument();
+	CClientDC dc(this);
+	int c;
+	CString y;
+	c = pDoc->A + pDoc->B;
+	y.Format(_T("A+B=%d"), c);
+	dc.TextOutW(300, 300, y);
+	CView::OnLButtonDown(nFlags, point);
+}
---
+++ 第三周实验题二/第二次实验题二/第二次实验题二View.h
@@ -0,0 +1,48 @@
+
+// 第二次实验题二View.h : C第二次实验题二View 类的接口
+//
+
+#pragma once
+
+
+class C第二次实验题二View : public CView
+{
+protected: // 仅从序列化创建
+	C第二次实验题二View();
+	DECLARE_DYNCREATE(C第二次实验题二View)
+
+// 特性
+public:
+	C第二次实验题二Doc* GetDocument() const;
+
+// 操作
+public:
+
+// 重写
+public:
+	virtual void OnDraw(CDC* pDC);  // 重写以绘制该视图
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+
+// 实现
+public:
+	virtual ~C第二次实验题二View();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 生成的消息映射函数
+protected:
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
+};
+
+#ifndef _DEBUG  // 第二次实验题二View.cpp 中的调试版本
+inline C第二次实验题二Doc* C第二次实验题二View::GetDocument() const
+   { return reinterpret_cast<C第二次实验题二Doc*>(m_pDocument); }
+#endif
+
