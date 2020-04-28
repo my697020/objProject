5ac84870050a8384b6343daa1333a9aa9742892c
my607020 <1693267656@qq.com>
2020-04-28 20:52:40 +0800

    "随机数的运算 在客户区画三个矩形Ａ, Ｂ和Ｃ，当鼠标左键在Ａ内点击时，产生一个随机数a并在Ａ内部显示；　当鼠标在Ｂ内左击时，也产生一个随机数b并在Ｂ内显示。如果鼠标左键不在Ａ、Ｂ内点击，在客户区显示”点击无效“。鼠标右键在Ｃ内点击时，在Ｃ内显示a+b的值"

---
+++ 第三周实验题三/第一次实验题三/第一次实验题三View.cpp
@@ -0,0 +1,105 @@
+
+// 第一次实验题三View.cpp : C第一次实验题三View 类的实现
+//
+
+#include "stdafx.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "第一次实验题三.h"
+#endif
+
+#include "第一次实验题三Doc.h"
+#include "第一次实验题三View.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// C第一次实验题三View
+
+IMPLEMENT_DYNCREATE(C第一次实验题三View, CView)
+
+BEGIN_MESSAGE_MAP(C第一次实验题三View, CView)
+	ON_WM_LBUTTONDOWN()
+	ON_WM_RBUTTONDOWN()
+END_MESSAGE_MAP()
+
+// C第一次实验题三View 构造/析构
+
+C第一次实验题三View::C第一次实验题三View()
+{
+	// TODO: 在此处添加构造代码
+
+}
+
+C第一次实验题三View::~C第一次实验题三View()
+{
+}
+
+BOOL C第一次实验题三View::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CView::PreCreateWindow(cs);
+}
+
+// C第一次实验题三View 绘制
+
+void C第一次实验题三View::OnDraw(CDC* /*pDC*/)
+{
+	C第一次实验题三Doc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 在此处为本机数据添加绘制代码
+}
+
+
+// C第一次实验题三View 诊断
+
+#ifdef _DEBUG
+void C第一次实验题三View::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void C第一次实验题三View::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+C第一次实验题三Doc* C第一次实验题三View::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(C第一次实验题三Doc)));
+	return (C第一次实验题三Doc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// C第一次实验题三View 消息处理程序
+
+
+void C第一次实验题三View::OnLButtonDown(UINT nFlags, CPoint point)
+{
+	C第一次实验题三Doc* pDoc = GetDocument();
+	pDoc->count++;
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+
+	CView::OnLButtonDown(nFlags, point);
+}
+
+
+void C第一次实验题三View::OnRButtonDown(UINT nFlags, CPoint point)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+	C第一次实验题三Doc* pDoc = GetDocument();
+	CClientDC dc(this);
+	CString s;
+	s.Format(_T("%d"), pDoc->count);
+	dc.TextOutW(200,200,s);
+	CView::OnRButtonDown(nFlags, point);
+}
