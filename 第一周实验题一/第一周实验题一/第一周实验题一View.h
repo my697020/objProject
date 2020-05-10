cf0353efd5af7a984fe3a40436ad512c834d18d0
my607020 <1693267656@qq.com>
2020-05-10 16:15:25 +0800

    1.生成程序框架，在Doc类中加字符数组成员(或CString)并初始化为学生名字， 鼠标左键点击显示名字

---
+++ 第一周实验题一/第一周实验题一/第一周实验题一View.h
@@ -0,0 +1,48 @@
+
+// 第一周实验题一View.h : C第一周实验题一View 类的接口
+//
+
+#pragma once
+
+
+class C第一周实验题一View : public CView
+{
+protected: // 仅从序列化创建
+	C第一周实验题一View();
+	DECLARE_DYNCREATE(C第一周实验题一View)
+
+// 特性
+public:
+	C第一周实验题一Doc* GetDocument() const;
+
+// 操作
+public:
+
+// 重写
+public:
+	virtual void OnDraw(CDC* pDC);  // 重写以绘制该视图
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+
+// 实现
+public:
+	virtual ~C第一周实验题一View();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 生成的消息映射函数
+protected:
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
+};
+
+#ifndef _DEBUG  // 第一周实验题一View.cpp 中的调试版本
+inline C第一周实验题一Doc* C第一周实验题一View::GetDocument() const
+   { return reinterpret_cast<C第一周实验题一Doc*>(m_pDocument); }
+#endif
+
