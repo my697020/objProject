beb84d3a133047cf9977d3f2c1afe791d116058b
my607020 <1693267656@qq.com>
2020-05-10 16:51:57 +0800

    "(1) 鼠标在客户区点击，画出以该点为中心，以随机数为半径的圆；"

---
+++ 第二周课堂实验题一/第二周课堂实验题一/第二周课堂实验题一View.cpp
@@ -0,0 +1,95 @@
+
+// 第二周课堂实验题一View.cpp : C第二周课堂实验题一View 类的实现
+//
+
+#include "stdafx.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "第二周课堂实验题一.h"
+#endif
+
+#include "第二周课堂实验题一Doc.h"
+#include "第二周课堂实验题一View.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// C第二周课堂实验题一View
+
+IMPLEMENT_DYNCREATE(C第二周课堂实验题一View, CView)
+
+BEGIN_MESSAGE_MAP(C第二周课堂实验题一View, CView)
+	ON_WM_LBUTTONDOWN()
+END_MESSAGE_MAP()
+
+// C第二周课堂实验题一View 构造/析构
+
+C第二周课堂实验题一View::C第二周课堂实验题一View()
+{
+	// TODO: 在此处添加构造代码
+
+}
+
+C第二周课堂实验题一View::~C第二周课堂实验题一View()
+{
+}
+
+BOOL C第二周课堂实验题一View::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CView::PreCreateWindow(cs);
+}
+
+// C第二周课堂实验题一View 绘制
+
+void C第二周课堂实验题一View::OnDraw(CDC* /*pDC*/)
+{
+	C第二周课堂实验题一Doc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 在此处为本机数据添加绘制代码
+}
+
+
+// C第二周课堂实验题一View 诊断
+
+#ifdef _DEBUG
+void C第二周课堂实验题一View::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void C第二周课堂实验题一View::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+C第二周课堂实验题一Doc* C第二周课堂实验题一View::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(C第二周课堂实验题一Doc)));
+	return (C第二周课堂实验题一Doc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// C第二周课堂实验题一View 消息处理程序
+
+
+void C第二周课堂实验题一View::OnLButtonDown(UINT nFlags, CPoint point)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+	int r = rand() % 100 + 10;
+	CRect cr(point.x - r, point.y - r, point.x + r, point.y + r);
+	CClientDC dc(this);
+	dc.Ellipse(cr);
+
+
+	CView::OnLButtonDown(nFlags, point);
+}
